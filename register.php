d781241ee884a89e3fa4e03b684c0bc7935fe7eb
Saša Stamenković <umpirsky@gmail.com>
2017-01-30 17:38:58 +0100

    Initial implementation

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Goutte\Client;
+
+$client = new Client();
+
+for ($i=0; $i < 1000; $i++) {
+    $crawler = $client->request('GET', 'http://www.jugoistok.com/prikazracuna/index.php/registracija_korisnika');
+    $form = $crawler->selectButton('Registrujte se')->form();
+    $crawler = $client->submit($form, [
+        'ime' => 'Ivan',
+        'prezime' => 'Golubovic',
+        'telefon' => '1234567',
+        'email' => sprintf('golub+%d@prc.com', $i),
+        'user_password' => 'opaaa',
+        'user_password_potvrda' => 'opaaa',
+        'uslovi_koriscenja' => 'da',
+    ]);
+
+    $crawler->filter('.alert')->each(function ($node) {
+        echo $node->text();
+    });
+}
