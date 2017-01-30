8c910926e495b6c806c275e3a34146e104bd0397
Milenko Jovanovic <milenko.jovanovic@exsila.ch>
2017-01-30 18:04:35 +0100

    Added python implementation

---
+++ register.py
@@ -0,0 +1,16 @@
+import string
+import random
+import requests
+
+url = 'http://www.jugoistok.com/prikazracuna/index.php/registracija_korisnika'
+
+# adjust range as needed
+for x in xrange(100,500):
+    name = 'Ivan'
+    lastname = 'Golubovic'
+    phone = '1234567'
+    email = 'golub'+str(random.randint(1,10000000))+'@jugoistok.com'
+    password = 'opaaa'
+    payload = {'ime': name, 'prezime':lastname, 'telefon':phone, 'email':email, 'user_password':password, 'user_password_potvrda':password, 'uslovi_koriscenja':'da','submit':'register'}
+    result = requests.post(url, data=payload)
+    print(email)
